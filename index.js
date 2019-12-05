3d0b7468c967642a6d987d9ee903e8bec2bdb7e0
Don-Stevenson <dsteven1@gmail.com>
2019-12-05 22:43:47 +0000

    Working up to fetchCoordsByIp

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+// const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIp } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:', ip);
+// });
+
+fetchCoordsByIp("66.207.199.230", (error, data) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log('It worked! Returned Coords' + data);
+});
+
