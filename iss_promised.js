28253e8a6443f5281ebd849cc3ebd525173848f2
Don-Stevenson <dsteven1@gmail.com>
2020-07-19 23:33:02 -0400

    update function syntax

--- iss_promised.js
+++ iss_promised.js
@@ -1,15 +1,15 @@
 const request = require("request-promise-native");
 
-const fetchMyIP = function () {
+const fetchMyIP = () => {
   return request("https://api.ipify.org?format=json");
 };
 
-const fetchCoordsByIP = function (body) {
+const fetchCoordsByIP = (body) => {
   const ip = JSON.parse(body).ip;
   return request(`https://ipvigilante.com/json/${ip}`);
 };
 
-const fetchISSFlyOverTimes = function (body) {
+const fetchISSFlyOverTimes = (body) => {
   const { latitude, longitude } = JSON.parse(body).data;
   const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
   return request(url);
